cefdc9304fcf0d03df80e90063bd856448da1e83
ARnikev <ARnikev@gmail.com>
2015-04-13 12:47:23 +0300

    some changes

--- src/main/java/ru/noname/gwtmvp/client/main.java
+++ src/main/java/ru/noname/gwtmvp/client/main.java
@@ -1,6 +1,7 @@
 package ru.noname.gwtmvp.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
---
+++ src/main/java/ru/noname/gwtmvp/client/views/PersonsGridPanel.java
@@ -0,0 +1,12 @@
+package ru.noname.gwtmvp.client.views;
+
+import com.sencha.gxt.widget.core.client.ContentPanel;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nikeshkin
+ * Date: 09.04.2015
+ * Time: 17:59
+ */
+public class PersonsGridPanel extends ContentPanel {
+}
